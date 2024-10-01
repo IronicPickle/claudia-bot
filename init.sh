fa636f643e53d23dd7de8670ec329df1eb76bf37
Nathan <nathanrath@live.co.uk>
2024-10-01 22:45:17 +0100

    updated init script

--- init.sh
+++ init.sh
@@ -1,5 +1,53 @@
+#!/bin/bash
+set -e
+
+printf "\e[95m_________ .__                   .___.__        
+\_   ___ \|  | _____   __ __  __| _/|__|____   
+/    \  \/|  | \__  \ |  |  \/ __ | |  \__  \  
+\     \___|  |__/ __ \|  |  / /_/ | |  |/ __ \_
+ \______  /____(____  /____/\____ | |__(____  /
+        \/          \/           \/         \/ \e[0m\n"
+
+printf "\e[1m> \e[33mClaudia Bot\e[0m\e[1m initialisation script\e[0m\n"
+
+BRANCH=$1
+USERNAME=$2
+TOKEN=$3
+CURR_DIR=$PWD
+
+# Check if branch is configured in args
+if [ ! $BRANCH ]; then
+  printf "  > Branch to init with:\e[0m\n"
+  read -p "  > " BRANCH
+fi
+
+printf "\n\e[1m> Starting initialisation with branch:\e[0m\n"
+printf "\e[34m  > ${BRANCH}\e[0m\n"
+
+# Setup shared enviro
+printf "\n\\e[1m> Installing \e[33mclaudia-shared\e[0m\e[1m environment\e[0m\n\n"
+
+mkdir -p ../claudia-shared
+cd ../claudia-shared
+
+if [ ! -d "./.git" ]; then
+  if [ $USERNAME ] && [ $TOKEN ]; then
+    git clone -b $BRANCH "https://${USERNAME}:${TOKEN}@github.com/IronicPickle/claudia-shared.git" .
+  else
+    git clone -b $BRANCH git@github.com:IronicPickle/claudia-shared.git .
+  fi
+fi
+
+cd $CURR_DIR
+
+# Setup main enviro
+printf "\n\\e[1m> Installing \e[33mclaudia-bot\e[0m\e[1m environment\e[0m\n\n"
+
+printf "\nCreating python virtual environment\n"
+python3 -m venv .venv
+
 if [ ! -e .env ]; then
-  echo "> Generating .env file"
+  printf "> Generating .env file"
   echo "DENO_ENV=development" >> .env
   echo "" >> .env
   echo "DISCORD_TOKEN=" >> .env
@@ -7,5 +55,7 @@ if [ ! -e .env ]; then
   echo "" >> .env
   echo "AUTH_SECRET=keyboard_cat" >> .env
 else
-  echo "> .env already exists, skipping..."
+  printf ".env already exists, skipping..."
 fi
+
+printf "\n\\e[1m> Initialisation done, \e[33mclaudia Bot\e[0m\e[1m ready\e[0m\n"
