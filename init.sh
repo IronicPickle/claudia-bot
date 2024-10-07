38af803af2edaee349efa257b885738a74a575ce
Nathan <nathanrath@live.co.uk>
2024-10-07 16:47:36 +0100

    moved venv installation out of init script, into deploy scripts

--- init.sh
+++ init.sh
@@ -13,6 +13,7 @@ printf "\e[1m> \e[33mClaudia Bot\e[0m\e[1m initialisation script\e[0m\n"
 BRANCH=$1
 USERNAME=$2
 TOKEN=$3
+NO_VENV=$4
 CURR_DIR=$PWD
 
 # Check if branch is configured in args
@@ -43,13 +44,10 @@ cd $CURR_DIR
 # Setup main enviro
 printf "\n\\e[1m> Installing \e[33mclaudia-bot\e[0m\e[1m environment\e[0m\n\n"
 
-printf "\nCreating python virtual environment\n"
-python3 -m venv .venv
-source .venv/bin/activate
-pip3 install yt-dlp
-pip3 install scdl
-pip3 install spotdl
-deactivate
+if [ "$NO_VENV" != "true" ]; then
+  printf "\nCreating python virtual environment\n"
+  python3 -m venv .venv
+fi
 
 if [ ! -e .env ]; then
   printf "Generating .env file"
